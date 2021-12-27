5f0526b286522eca3df0d23c8999b27fe722675a
nawfel.sghaier <sghaier.nawfel@hotmail.com>
2021-12-27 02:47:25 +0100

    section 8

---
+++ client/src/app/Models/products.ts
@@ -0,0 +1,9 @@
+export interface IProduct {
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+    picutreUrl: string;
+    productType: string;
+    productBrand: string;
+}
